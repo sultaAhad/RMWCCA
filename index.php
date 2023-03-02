6a7c328a99d0d4baabe2fe1497b3d4ae291f2137
sultan Ahad <101328284+sultaAhad@users.noreply.github.com>
2023-03-02 22:36:44 +0500

    add

--- index.php
+++ index.php
@@ -63,10 +63,134 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="about-us-wrapper-img">
-                    <img src="" class="img-fluid" alt="">
+                    <img src="img/about-us.png" class="img-fluid" alt="">
                 </div>
             </div>
-            <div class="col-lg-6"></div>
+            <div class="col-lg-6">
+                <div class="about-heading">
+                    <h4>About Us</h4>
+                    <p>RMWCCA exists to unify and align the Colorado Walls and Ceilings industry. Through
+                        representation, education, and leadership we strive to create a commerce of dominance for
+                        superior products and craftsmanship.
+                    </p>
+                    <p>RMWCCA was a long-time vision of several individuals who have worked with EIFS & Stucco for more
+                        than 25 years. Upon arriving in Colorado there seem to be no one organization that brought
+                        competitors, experts, and educators together under one roof. Here at RMWCCA we recognize that
+                        although we may be competitors at the end of the day, it is the collective industry that must
+                        move forward, for all to benefit.
+                    </p>
+                    <p>Competitors or allies we all understand there is a lack of leadership as well as oversight in our
+                        industry despite the fact our systems are more eco/energy efficient and design friendly
+                        claddings on the market today.
+                    </p>
+                    <p>Our goal is to improve workmanship and educate contractors on how to utilize proper installation
+                        standards for stucco, stucco with ci, MVES as well as full on EIF systems to ultimately reclaim
+                        the market from inferior cladding systems in Colorado. By joining forces as allies we will not
+                        only have fun but will promote honorable relationships among businesses and we will dominate the
+                        wall and ceiling industry.</p>
+                    <div class="para-img">
+                        <img src="img/advancelogo.png" class="img-fluid" alt="">
+                        <a class="btn green" href="">Learn More <span> <i class="fa fa-arrow-right"
+                                    aria-hidden="true"></i>
+                            </span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="service">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="service-heading text-center">
+                    <h4>Services</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Educational</h3>
+                        <img src="img/service.png" class="img-fluid" alt="">
+                    </div>
+                    <p>RMWCCA will provide classes from a variety of well-established industry experts. We will focus on
+                        education that is By the Book.</p>
+                </div>
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Magazine</h3>
+                        <img src="img/service1.png" class="img-fluid" alt="">
+                    </div>
+                    <p>RMWCCA will provide classes from a variety of well-established industry experts. We will focus on
+                        education that is By the Book.</p>
+                </div>
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Events</h3>
+                        <img src="img/service5.png" class="img-fluid" alt="">
+                    </div>
+                    <p>RMWCCA will provide classes from a variety of well-established industry experts. We will focus on
+                        education that is By the Book.</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <img src="img/building.png" class="img-fluid" alt="">
+            </div>
+            <div class="col-lg-4">
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Technical</h3>
+                        <img src="img/service4.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Job site observation reports</h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> RFI’s, VE’s</h6>
+                        </div>
+                        <div class="col-lg-7">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Observation Reports</h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Plan Reviews</h6>
+                        </div>
+                        <div class="col-lg-7">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Job Meetings</h6>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Job Meetings</h6>
+                        </div>
+                        <div class="col-lg-7">
+                            <h6> <img src="img/star.png" class="img-fluid" alt=""> Submittals</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Legal</h3>
+                        <img src="img/service3.png" class="img-fluid" alt="">
+                    </div>
+                    <p>Coming soon</p>
+                </div>
+                <div class="service-wrapper-detail">
+                    <div class="heading-main-service">
+                        <h3>Giving Back</h3>
+                        <img src="img/service2.png" class="img-fluid" alt="">
+                    </div>
+                    <p>RMWCCA will establish a scholarship fund to encourage education that ensures the future of our
+                        industry. We will proudly sponsor our Veterans and First Responders through American Heroes in
+                        Action. </p>
+                </div>
+            </div>
         </div>
     </div>
 </section>
